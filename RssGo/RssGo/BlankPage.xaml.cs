695dc330abdf3a743d393c4d5cd2001ca97dfa13
unknown <Jiri@EliteBook8540w.(none)>
2012-04-13 17:24:43 +0200

    ignore ignored files please

--- RssGo/RssGo/BlankPage.xaml.cs
+++ RssGo/RssGo/BlankPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using RssGoLibrary;
 
 namespace RssGo
 {
@@ -23,10 +24,25 @@ namespace RssGo
 
 
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             DataContext = App.FeedDataSource;
-          
+            //Fill="{Binding Source={StaticResource SettingsWithBrush}, Path=Brush}"
         }
+
+        private void ItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            
+            RssFeedItem feedItem = e.AddedItems[0] as RssFeedItem;
+            if (feedItem != null)
+            {
+                // Navigate the WebView to the blog post content HTML string.
+                ContentView.NavigateToString(feedItem.Content);
+                ContentView1.NavigateToString(feedItem.Content);
+                ContentView2.NavigateToString(feedItem.Content);
+            }
+        }
+
     }
 }
